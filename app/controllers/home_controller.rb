9493c04b66884b710868f54b95a3dd50f51631dd
madhu-patidar <madhusudan.patidar@neosofttech.com>
2017-01-31 20:37:07 +0530

    change in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @banners = Banner.all
     @top_brands = Brand.take(10)
     @categories = Category.all
-    @category = Category.sub_categories.first
+    @category = Category.first.sub_categories.first
     @brands = Brand.all
     
     if params[:sub_category_id].present?
