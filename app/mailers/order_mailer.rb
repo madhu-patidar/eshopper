df6f52ccfc1ad18988b0f199218fb999a06a017d
madhu-patidar <madhusudan.patidar@neosofttech.com>
2017-02-08 21:37:51 +0530

    remove log image from order mail and welcome mail

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,7 +5,7 @@ class OrderMailer < ApplicationMailer
     @customer = customer
     @customer_order = customer_order
     @url  = 'http://example.com/login'
-    attachments.inline["logo.png"] = File.read("/home/webwerks/Assignments/training/ROR/live_project/eshopper/app/assets/images/home/logo.png")
+    #attachments.inline["logo.png"] = File.read("/home/webwerks/Assignments/training/ROR/live_project/eshopper/app/assets/images/home/logo.png")
     mail(to: @customer.email, subject: 'Successfully Order placed')
   end
 end
