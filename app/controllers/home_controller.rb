577998c04ed920a87638e9bdebcc2dbb50e07d54
madhu-patidar <madhusudan.patidar@neosofttech.com>
2017-02-10 21:09:17 +0530

    made changes in Home page for left side bar and cart item count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,14 @@ class HomeController < ApplicationController
     @banners = Banner.all
     @top_brands = Brand.take(10)
     @categories = Category.all
-    @category = Category.first
+    @category = Category.last
     @brands = Brand.all
     
     if params[:sub_category_id].present?
       @sub = Category.find(params[:sub_category_id])
       @products = Product.where(category_id: params[:sub_category_id])
-    else
-      @products = Product.where(category_id: @category.sub_categories.first.id)
+    elsif Category.first.present? && Category.first.sub_categories.present?
+      @products = Product.where(category_id:  Category.first.sub_categories.first.id)
     end
 
   end
