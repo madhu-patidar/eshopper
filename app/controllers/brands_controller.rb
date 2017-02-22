5eedabf39a51e59c4234dcf20d228dabd769660a
madhu-patidar <madhusudan.patidar@neosofttech.com>
2017-02-22 12:44:13 +0530

    made changes in branda controller

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,5 +1,5 @@
 class BrandsController < ApplicationController
-  before_action :set_brand, only: [:show, :edit, :update, :destroy]
+  before_action :set_brand, only: [:show]
 
   # GET /brands
   # GET /brands.json
@@ -13,14 +13,11 @@ class BrandsController < ApplicationController
     @products = @brand.products
   end
 
-  # GET /brands/1
-  # GET /brands/1.json
   def show
     @top_brands = Brand.take(10)
     @brands = Brand.all
     @categories = Category.all
     @category = Category.find(params[:category_id])
-    @brand = Brand.find(params[:id])
     @sub_categories = @brand.categories.where(category_id: params[:category_id]).order(:id)
     if params.has_key?(:sub_category_id)
       @sub1 = Category.find(params[:sub_category_id])
@@ -38,53 +35,6 @@ class BrandsController < ApplicationController
    
   end
 
-  # GET /brands/new
-  def new
-    @brand = Brand.new
-  end
-
-  # GET /brands/1/edit
-  def edit
-  end
-
-  # POST /brands
-  # POST /brands.json
-  def create
-    @brand = Brand.new(brand_params)
-    respond_to do |format|
-      if @brand.save
-        format.html { redirect_to @brand, notice: 'Brand was successfully created.' }
-        format.json { render :show, status: :created, location: @brand }
-      else
-        format.html { render :new }
-        format.json { render json: @brand.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /brands/1
-  # PATCH/PUT /brands/1.json
-  def update
-    respond_to do |format|
-      if @brand.update(brand_params)
-        format.html { redirect_to @brand, notice: 'Brand was successfully updated.' }
-        format.json { render :show, status: :ok, location: @brand }
-      else
-        format.html { render :edit }
-        format.json { render json: @brand.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /brands/1
-  # DELETE /brands/1.json
-  def destroy
-    @brand.destroy
-    respond_to do |format|
-      format.html { redirect_to brands_url, notice: 'Brand was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
